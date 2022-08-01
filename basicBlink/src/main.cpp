c8ff817568a62bda13934ffb53e45e66593ae021
ndtc <yujinh@126.com>
2022-08-02 00:57:33 +0800

    Add files via upload

---
+++ basicBlink/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+int led = 25;
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+}
